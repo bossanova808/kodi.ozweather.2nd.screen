9e35cec02bf5b72ae08bf9188cc1b94e7c648aa4
Jeremy Daalder <jd@imagescience.com.au>
2023-10-07 10:15:08 +1100

    Commit test from new machine

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,4 @@
 
 call npm install
 call npm run build
-robocopy "C:\Git\Kodi\dash.vite.alpine.tailwind\dist" "\\homeserver3\Docker\config\swag\vhosts\deploy.kodidash" /MIR /XD "C:\Git\Kodi\dash.vite.alpine.tailwind\dist\.git"
+robocopy "E:\Git\Kodi\dash.vite.alpine.tailwind\dist" "\\homeserver3\Docker\config\swag\vhosts\deploy.kodidash" /MIR /XD "E:\Git\Kodi\dash.vite.alpine.tailwind\dist\.git"
