0a4ed32fe4d3b677ad5e4d938ad90e558f106655
Jeremy Daalder <jd@imagescience.com.au>
2023-11-09 08:20:38 +1100

    Update favicon to svg and deploy paths

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,4 @@
 
 call npm install
 call npm run build
-robocopy "E:\Git\Kodi\dash.vite.alpine.tailwind\dist" "\\homeserver3\Docker\config\swag\vhosts\deploy.kodidash" /MIR /XD "E:\Git\Kodi\dash.vite.alpine.tailwind\dist\.git"
+robocopy "E:\Git\Dashboard\dash.vite.alpine.tailwind\dist" "\\homeserver3\Docker\config\swag\vhosts\deploy.kodidash" /MIR /XD "E:\Git\Kodi\dash.vite.alpine.tailwind\dist\.git"
