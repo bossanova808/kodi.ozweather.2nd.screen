e271dd9006b6b071d9a4abc471528ef1c8a38477
Jeremy Daalder <jd@imagescience.com.au>
2023-11-26 13:32:11 +1100

    Remove old deploy script

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-echo "This is not in use anymore - manual FPT to infinityfree for now..."
-exit
-
-@echo off
-
-call npm install
-call npm run build
-robocopy "E:\Git\Dashboard\dash.vite.alpine.tailwind\dist" "\\homeserver3\Docker\config\swag\vhosts\deploy.kodidash" /MIR /XD "E:\Git\Kodi\dash.vite.alpine.tailwind\dist\.git"
