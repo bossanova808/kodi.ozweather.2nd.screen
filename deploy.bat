60e49d72dcccb60380fec835189cc406ae9c2e8f
Jeremy Daalder <jd@imagescience.com.au>
2024-08-19 12:37:58 +1000

    Improve deploy.bat so I can do from any machine & exclude .git folder, plus npm update

--- deploy.bat
+++ deploy.bat
@@ -4,10 +4,11 @@ call npm install
 call npm run build
 
 echo "Deploying to: homeserver3"
-robocopy /MIR "E:\Git\Dashboard\kodi.ozweather.2nd.screen\dist" "C:\mnt\homeserver3\home\docker\config\nginx\www"
+robocopy /MIR "dist" "C:\mnt\homeserver3\home\docker\config\nginx\www" /xd ".git"
 
 echo "Deploying to: bossanova808.net"
-robocopy /MIR "E:\Git\Dashboard\kodi.ozweather.2nd.screen\dist" "C:\mnt\bossanova808\home\docker\config\swag\www\dash.bossanova808.net"
+robocopy /MIR "dist" "C:\mnt\bossanova808\home\docker\config\swag\www\dash.bossanova808.net" /xd ".git"
 
 echo "Deploying to: adelaideserver"
-robocopy /MIR "E:\Git\Dashboard\kodi.ozweather.2nd.screen\dist" "C:\mnt\adelaideserver\home\docker\config\nginx\www"
+robocopy /MIR "dist" "C:\mnt\adelaideserver\home\docker\config\nginx\www" /xd ".git"
+
