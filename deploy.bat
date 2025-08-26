c41482f4eb83b5db3d267466a08f13574e4ec646
Jeremy Daalder <jeremy@daalders.net>
2025-08-26 21:32:33 +0930

    NPM Update

--- deploy.bat
+++ deploy.bat
@@ -9,6 +9,6 @@ robocopy /MIR "dist" "C:\mnt\homeserver3\home\docker\config\nginx\www" /xd ".git
 echo "Deploying to: bossanova808.net"
 robocopy /MIR "dist" "C:\mnt\bossanova808\home\docker\config\swag\www\dash.bossanova808.net" /xd ".git"
 
-rem echo "Deploying to: adelaideserver"
-rem robocopy /MIR "dist" "C:\mnt\adelaideserver\home\docker\config\nginx\www" /xd ".git"
+echo "Deploying to: adelaide-services"
+robocopy /MIR "dist" "C:\mnt\adelaide-services\home\docker\config\nginx\www" /xd ".git"
 
