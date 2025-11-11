99fb16dc7fab3013960ee85c62d15364f759fee2
Jeremy Daalder <jeremy@daalders.net>
2025-11-11 12:38:31 +1030

    CR: Remove not needed quotes

--- deploy-adelaide.bat
+++ deploy-adelaide.bat
@@ -3,6 +3,6 @@
 call npm install
 call npm run build
 
-echo "Deploying to: adelaide-services"
+echo Deploying to: adelaide-services
 robocopy /MIR "dist" "C:\mnt\adelaide-services\home\appdata\nginx\www" /xd ".git"
 
--- deploy-dev.bat
+++ deploy-dev.bat
@@ -3,7 +3,7 @@
 call npm install
 call npm run build
 
-echo "Deploying to: dashdev.bossanova808.net"
+echo Deploying to: dashdev.bossanova808.net
 robocopy /MIR "dist" "C:\mnt\bossanova808-public-hs14\home\appdata\nginx-dev\www" /xd ".git"
 
 
