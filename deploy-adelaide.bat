1172133321ada8acb04df693d974c6b6ca152bca
Jeremy Daalder <jeremy@daalders.net>
2025-11-11 12:36:41 +1030

    Update deploy-adelaide.bat

--- deploy-adelaide.bat
+++ deploy-adelaide.bat
@@ -4,5 +4,5 @@ call npm install
 call npm run build
 
 echo "Deploying to: adelaide-services"
-robocopy /MIR "dist" "C:\mnt\adelaide-services\home\docker\config\nginx\www" /xd ".git"
+robocopy /MIR "dist" "C:\mnt\adelaide-services\home\appdata\nginx\www" /xd ".git"
 
